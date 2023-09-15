e6a2e445aa8b11618fd7d6ce89646d425003523f
HudinetsRV <hudinetsrv@gebank.ru>
2023-09-15 22:54:10 +0300

    alfa  version

---
+++ ChatGptDiscordBot/GPTContracts/ChatGptApiRequest.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace ChatGptDiscordBot.GPTContracts;
+
+public class ChatGptApiRequest
+{
+    public ChatGptApiRequest(string model, params string[] messages)
+    {
+        Model = model;
+        Messages = new GptMessage[messages.Length];
+        for (var i = 0; i < messages.Length; i++)
+        {
+            Messages[i] = new GptMessage("user", messages[i]);
+        }
+    }
+    [JsonProperty("model")]
+    public string Model { get; set; }
+    [JsonProperty("messages")]
+    public GptMessage[] Messages { get; set; }
+}
+
+public class GptMessage
+{
+    public GptMessage(string role, string message)
+    {
+        Role = role;
+        Content = message;
+    }
+    [JsonProperty("role")]
+    public string Role { get; set; }
+    [JsonProperty("content")]
+    public string Content { get; set; }
+}
---
+++ ChatGptDiscordBot/OpenAIChatService.cs
@@ -0,0 +1,27 @@
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+
+namespace ChatGptDiscordBot;
+
+public class OpenAIChatService
+{
+    private static OpenAIAPI _api;
+
+    static OpenAIChatService()
+    {
+        _api = new OpenAIAPI(new APIAuthentication(Environment.GetEnvironmentVariable("OPENAI_TOKEN")));
+    }
+
+    static public IAsyncEnumerable<ChatResult> GetGPT35Response(string request)
+    {
+        return _api.Chat.StreamChatEnumerableAsync(new List<ChatMessage>(new ChatMessage[]
+            { new ChatMessage(ChatMessageRole.User, request) }));
+
+    }
+
+    public static Conversation CreateConversation(string model)
+    {
+        return _api.Chat.CreateConversation(new ChatRequest(){Model = model});
+    }
+}
